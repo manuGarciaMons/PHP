06af861885ec44910be0a6840b8346ac907ac1bb
Manuela <mgarciamons@gmail.com>
2020-03-25 16:28:38 +0100

    Ejercicio 7

---
+++ EJERCICIOS/Ejercicio7.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * 
+ * mostrar por pantalla los numeros que se encuentre
+ * entre dos numeros  que sean impares
+ * 
+ */
+
+if (isset($_GET['numero1'] ) && isset($_GET['numero2'])) {
+    
+$numero1 = $_GET['numero1'];
+$numero2 = $_GET['numero2'];
+if ($numero1 < $numero2) {
+    for ($i=$numero1; $i <=$numero2 ; $i++) { 
+
+        if ($i%2 !=0) {
+            echo "<h4> $i Es Impar </h4>";
+        }else {
+            echo "<h4> $i Es Par </h4>";
+        }
+        
+    }
+    
+}else{
+    echo "<h1> el numero 1 debe ser menor al numero 2</h1>";
+}
+
+}else{
+echo "<h1> Los parametros get no existen</h1>";
+}
+?>
