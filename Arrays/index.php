640c13ca12792c4b1afd62a34cdf0672f116ee69
Manuela <mgarciamons@gmail.com>
2020-04-03 20:11:23 -0500

    arrays

---
+++ Arrays/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+/**
+ * Array
+ * Colección de datos/valores/variable
+ * bajo un unico nombre y para acceder  a esos valores 
+ * se puede usar un indice numero o alfanuumerico
+ * 
+ */
+
+ $peliculas =array('batman', 'superman','señor de los anillos');
+ var_dump($peliculas);
+$cantantes =['Drake', 'tylor swift', 'jesse y joy'];
+var_dump($cantantes);
+echo $peliculas[0];
+echo "<br/>";
+echo $cantantes[1];
+
+
+
+?>
