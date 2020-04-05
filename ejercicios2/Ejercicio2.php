60098dd9e3927289ccb27ce1713d2d7922a2b95a
Manuela <mgarciamons@gmail.com>
2020-04-05 17:06:14 -0500

    ejercicio2

---
+++ ejercicios2/Ejercicio2.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Escribir un programa que añada valores a un array mietras que su longitud
+ * sea menor a 120 
+ * y luego mostrarlo por pantalla 
+ * 
+ * 
+ */
+
+ $coleccion = array();
+
+ for ($i=0; $i <120 ; $i++) { 
+     array_push(($coleccion. "elemento- " .$i));
+
+ }
+ echo ($coleccion);
+?>
