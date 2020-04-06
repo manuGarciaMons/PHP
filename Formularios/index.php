72d0af7cf0d8d42075770d75279040f3d4ad37c6
Manuela <mgarciamons@gmail.com>
2020-04-06 15:57:51 -0500

    formularios

---
+++ Formularios/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario php</title>
+</head>
+
+<body>
+    <h1>Formulario</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="nombre">Nombre:</label>
+        <p><input type="text" name="nombre" id="" placeholder="Nombre"></p>
+
+        <label for="apellido">Apellido:</label>
+        <p> <input type="text" name="apellido" id="" placeholder="Apellido"></p>
+
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+
+</html>
