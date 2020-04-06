18b95d0870074b2365073bfe50dab29bc689d97c
Manuela <mgarciamons@gmail.com>
2020-04-06 18:54:27 -0500

    formularios

--- Formularios/index.php
+++ Formularios/index.php
@@ -16,6 +16,44 @@
         <label for="apellido">Apellido:</label>
         <p> <input type="text" name="apellido" id="" placeholder="Apellido"></p>
 
+        <label for="boton">Boton:</label>
+        <p> <input type="button" name="boton" id="" value="clickMe"></p>
+
+        <label for="sexo">Sexo:</label>
+        <p>
+            Hombre: <input type="checkbox" name="sexo" id="" value="hombre">
+            Mujer: <input type="checkbox" name="sexo" id="" value="mujer">
+        </p>
+
+        <label for="color">Color:</label>
+        <p> <input type="color" name="color" id=""></p>
+
+        <label for="fecha">Fecha:</label>
+        <p> <input type="date" name="fecha" id=""></p>
+
+        <label for="correo">Email:</label>
+        <p> <input type="email" name="correo" id=""></p>
+
+        <label for="archivo">Archivo:</label>
+        <p> <input type="file" name="archivo" id="" multiple="multiple"></p>
+
+        <label for="numero">Numero:</label>
+        <p> <input type="number" name="numero" id=""></p>
+
+        <label for="password">Contraseña:</label>
+        <p> <input type="password" name="password" id=""></p>
+
+        <label for="continente">Continente:</label>
+        <p>
+            America del sur <input type="radio" name="continente" id="" value="America del sur">
+            Europa <input type="radio" name="continente" id="" value="Europa">
+            Asia <input type="radio" name="continente" id="" value="Asia">
+        </p>
+
+        <label for="web">Pagina web:</label>
+        <p> <input type="url" name="web" id=""></p>
+        
+
         <input type="submit" value="Enviar">
     </form>
 </body>
