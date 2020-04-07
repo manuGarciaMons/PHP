8212303888f27d7e91151a89d3d3d6114af84eac
Manuela <mgarciamons@gmail.com>
2020-04-07 12:34:22 -0500

    ejercicio 1, bloque 3

---
+++ Ejercicios3/ejercicio1.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 
+ * crear una sesion que aumente su valor en uno
+ * o diminuta en uno en funcion de si
+ * el parametro get counter esta a uno o a cero
+ * 
+ */
+
+ session_start();
+
+ if (!isset($_SESSION['numero'])) {
+     $_SESSION['numero']=0;
+ }
+
+ if (isset($_GET['counter']) && $_GET['counter'] == 1) {
+     $_SESSION['numero']++;
+ }
+
+ if (isset($_GET['counter']) && $_GET['counter'] == 0) {
+    $_SESSION['numero']--;
+}
+
+ ?>
+<h1>El valor de la sesion es : <?= $_SESSION['numero']?></h1>
+ <a href="ejercicio2.php?counter=1">Aumentar el valor</a><br/>
+ <a href="ejercicio1.php?counter=0">Disminuir el valor</a>
