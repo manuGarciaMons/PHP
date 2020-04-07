c8d40c1127b9b1390f2944bf00894a172447624c
Manuela <mgarciamons@gmail.com>
2020-04-07 13:16:52 -0500

    cambios

--- Ejercicios3/ejercicio3.php
+++ Ejercicios3/ejercicio3.php
@@ -10,21 +10,21 @@
 
 $resultado = false;
 if (isset($_POST['n1']) && isset($_POST['n2'])) {
-   
+
     if (isset($_POST['sumar'])) {
-        $resultado = "  El resultado es: ".($_POST['n1'] + $_POST['n2']);
+        $resultado = "  El resultado es: " . ($_POST['n1'] + $_POST['n2']);
     }
 
     if (isset($_POST['restar'])) {
-        $resultado = "  El resultado es: ".($_POST['n1'] - $_POST['n2']);
+        $resultado = "  El resultado es: " . ($_POST['n1'] - $_POST['n2']);
     }
 
     if (isset($_POST['multiplicar'])) {
-        $resultado = "  El resultado es: ".($_POST['n1'] * $_POST['n2']);
+        $resultado = "  El resultado es: " . ($_POST['n1'] * $_POST['n2']);
     }
 
     if (isset($_POST['dividir'])) {
-        $resultado = "  El resultado es: ".($_POST['n1'] / $_POST['n2']);
+        $resultado = "  El resultado es: " . ($_POST['n1'] / $_POST['n2']);
     }
 }
 
@@ -44,11 +44,11 @@ if (isset($_POST['n1']) && isset($_POST['n2'])) {
     <h1>Calculadora PHP</h1>
     <form action="" method="post">
 
-        <label for="n1">Numero 1</label><br/>
-        <input type="number" name="n1" id=""><br/>
+        <label for="n1">Numero 1</label><br />
+        <input type="number" name="n1" id=""><br />
 
-        <label for="n2">Numero 2</label><br/>
-        <input type="number" name="n2" id=""><br/><br/>
+        <label for="n2">Numero 2</label><br />
+        <input type="number" name="n2" id=""><br /><br />
 
         <input type="submit" value="Sumar" name="sumar">
         <input type="submit" value="Restar" name="restar">
