17392cbee1eb29187a3ecf892cfa7960d0388794
Manuela <mgarciamons@gmail.com>
2020-04-06 20:58:25 -0500

    funciones para trabajar con ficheros

--- Ficheros/index.php
+++ Ficheros/index.php
@@ -14,3 +14,22 @@ fwrite($archivo, " soy un texto metido desde php");
 
 // cerrar archivo
 fclose($archivo);
+
+
+// copiar el fichero
+copy('fichero_texto.txt', 'fichero.txt') or die("error copiar");
+
+// renombrar un fichero 
+rename('fichero_copiado.txt', 'archivito_recopiadito.txt' );
+
+// eliminar fichero
+
+unlink('fichero_texto.txt') or die ('error al borrar el archivo');
+
+
+// si existe un archivo 
+if (file_exists('fichero_texto.txt')) {
+    echo "El archivo existe";
+}else{
+    echo "El archivo no existe";
+}
