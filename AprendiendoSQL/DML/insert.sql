be57314b7a1b4f9d07e9b385a9f187f3e42a1993
Manuela <mgarciamons@gmail.com>
2020-04-08 13:48:09 -0500

    inset into

---
+++ AprendiendoSQL/DML/insert.sql
@@ -0,0 +1,13 @@
+#INSERTAR NUEVOS REGISTROS#
+
+INSERT INTO usuarios VALUES(null, 'Manuela', 'Garcia Monsalve', 'dyg9812@gmail.com', '12345', '2020-05-01');
+INSERT INTO usuarios VALUES(null, 'Nancy', 'Garcia Monsalve', 'nancy@gmail.com', '12345', '2020-04-01');
+INSERT INTO usuarios VALUES(null, 'Andrea', 'Garcia Monsalve', 'yeimeand@gmail.com', '12345', '2020-02-01');
+INSERT INTO usuarios VALUES(null, 'Nora ', 'Garcia Monsalve', 'noel672@gmail.com', '12345', '2020-09-01');
+
+
+#INSERTAR FILAS SOLO CON CIERTAS COLUMNAS#
+INSERT INTO usuarios(email,password) VALUES('dyg9812@gmai.com', '12345');
+
+
+
