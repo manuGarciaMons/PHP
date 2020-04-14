27f34b1512f129243d6dcd08ce532e8c012e280f
Manuela <mgarciamons@gmail.com>
2020-04-14 16:03:27 -0500

    ejercicios 10,11,12 de consecionario database

---
+++ AprendiendoSQL/Ejercicios/ejercicio11.sql
@@ -0,0 +1,6 @@
+/*
+Visualizar todos los cargos y el numero de vendedores que hay en cada cargo
+
+*/
+
+SELECT cargo, COUNT(id) FROM vendedores GROUP BY cargo ORDER BY COUNT(id) DESC;
---
+++ AprendiendoSQL/Ejercicios/ejercicio12.sql
@@ -0,0 +1,5 @@
+/*
+Conseguir la masa salarial del consecionario(anual)
+
+*/
+SELECT SUM(sueldo) AS 'Masa salarial' FROM  vendedores; 
