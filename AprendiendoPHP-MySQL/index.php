2bd797bb24f956d72e65e00a6aa7db0ad88182c9
Manuela <mgarciamons@gmail.com>
2020-04-16 15:10:50 -0500

    conexion Database

---
+++ AprendiendoPHP-MySQL/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * CONEXION A LA BASE DE DATOS
+ */
+    $conexion = mysqli_connect('localhost', 'root', '', 'phpmysql', '33065');
+
+    // comprobar si la conexion es correcta
+    if (mysqli_connect_errno()) {
+        echo "La conexion a la base de datos msql ha fallado:" .mysqli_connect_error();
+    }else{
+        echo "Conexión realizada correctamente";
+    }
+
+    // consulta para configurar la codificacion de caracteres
+
+    mysqli_query($conexion, "SET NAMES 'utf8' ");
+
+    // consulta SELECT desde php
+
+    $query =mysqli_query($conexion, "SELECT * FROM notas ");
+    while ($nota = mysqli_fetch_assoc($query)) {
+        echo "<h2>".$nota['titulo']. "</h2>";
+        echo $nota['descripcion']. "<br/>";
+    }
