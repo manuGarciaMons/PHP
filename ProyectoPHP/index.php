ae74fd281bd54add8ea99bcaa81a48aedbb0f719
Manuela <mgarciamons@gmail.com>
2020-04-17 17:42:54 -0500

    inicio del proyecto

---
+++ ProyectoPHP/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog de VideoJuegos</title>
+</head>
+
+<body>
+    <!-- CABECERA -->
+    <header id="header">
+        <!--logo -->
+        <div id="logo">
+            <a href="index.php">
+                Blog de Video Juegos
+            </a>
+        </div>
+
+
+        <!-- MENU -->
+        <nav id="nav">
+
+            <ul>
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+
+                <li>
+                    <a href="index.php">Categoria 1</a>
+                </li>
+
+                <li>
+                    <a href="index.php">Categoria 2</a>
+                </li>
+
+                <li>
+                    <a href="index.php">Categoria 3</a>
+                </li>
+
+                <li>
+                    <a href="index.php">Categoria 4</a>
+                </li>
+
+                <li>
+                    <a href="index.php">Sobre mi</a>
+                </li>
+
+                <li>
+                    <a href="index.php">Contacto</a>
+                </li>
+            </ul>
+
+        </nav>
+    </header>
+
+    <!-- BARRA LATERAL -->
+
+    <!-- CAJA PRINCIPAL -->
+
+    <!-- PIE DE PAGINA -->
+</body>
+
+</html>
