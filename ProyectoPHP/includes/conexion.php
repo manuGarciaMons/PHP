b89284f505a9d9b58de7b17ef8d2bab4f709538d
Manuela <mgarciamons@gmail.com>
2020-04-21 14:09:39 -0500

    validacion de datos del registro de usuarios

--- ProyectoPHP/includes/conexion.php
+++ ProyectoPHP/includes/conexion.php
@@ -8,3 +8,6 @@ $database = 'blogmaster';
 $db= mysqli_connect($server, $username, $password,$database);
 
 mysqli_query($db, "SET NAMES 'utf8'");
+
+// inciar la sesion
+session_start();
--- ProyectoPHP/includes/lateral.php
+++ ProyectoPHP/includes/lateral.php
@@ -1,3 +1,4 @@
+<?php require_once 'includes/helpers.php'; ?>
 <aside id="sidebar">
 
     <div id="login" class="bloque">
@@ -16,23 +17,33 @@
 
 
     <div id="register" class="bloque">
+        
         <h3>Registrate</h3>
         <form action="registro.php" method="post">
 
             <label for="nombre">Nombre</label>
             <input type="text" name="nombre" id="" />
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'nombre')  : ''; ?>
+
 
             <label for="apellidos">Apellidos</label>
             <input type="text" name="apellidos" id="" />
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'apellidos')  : ''; ?>
+
 
             <label for="email">Email</label>
             <input type="email" name="email" id="" />
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'email')  : ''; ?>
+
 
             <label for="password">Password</label>
             <input type="password" name="password" id="" />
+            <?php echo isset($_SESSION['errores']) ? mostrarError($_SESSION['errores'], 'password')  : ''; ?>
 
-            <input type="submit" value="Entrar" />
+
+            <input type="submit" name="submit" value="Entrar" />
         </form>
+        <?php borrarErrores(); ?>
     </div>
 
 </aside>
