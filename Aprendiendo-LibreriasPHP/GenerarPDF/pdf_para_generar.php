e36c902b095dfd6fc98864c5322972d7bde686ca
Manuela <mgarciamons@gmail.com>
2020-05-17 19:56:43 -0500

    generar pdf libreria: html2pdf

---
+++ Aprendiendo-LibreriasPHP/GenerarPDF/pdf_para_generar.php
@@ -0,0 +1,41 @@
+<html lang="es">
+<head>
+
+    <meta charset="UTF-8"/>
+    <title>Generar PDF</title>
+
+    <style type="text/css">
+
+        body{
+            font-family: "Century Gothic";
+        }
+        h1{
+            background: #00AAFF;
+            text-align: center;
+        }
+        p{
+            font-size: 20px;
+
+        }
+        ul{
+            background: #66AACC;
+            color:white;
+        }
+    </style>
+</head>
+<body>
+<h1>Datos de su factura</h1>
+<p>Usuario: Manuela Garcia Monsalve</p>
+<ul>
+    <li>Pantene pro V</li>
+    <li>Crema dental colgate</li>
+    <li>Jabon Protex</li>
+    <li>Desinfectante 50ml</li>
+    <li>Desodorante lady speedd stick</li>
+    <li>Antibacterial de 1l</li>
+</ul>
+
+
+</body>
+
+</html>
