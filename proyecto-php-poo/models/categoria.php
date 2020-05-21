724316014a090dbdc1e288620cced3e554cb351a
Manuela <mgarciamons@gmail.com>
2020-05-21 17:43:56 -0500

    gestionar categorias

---
+++ proyecto-php-poo/models/categoria.php
@@ -0,0 +1,59 @@
+<?php
+
+class Categoria
+{
+    private $id;
+    private $nombre;
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = Database::connect();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Categoria
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     * @return Categoria
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    public function getAll(){
+      $categorias = $this->db->query("SELECT * FROM categorias;");
+
+      return $categorias;
+    }
+
+
+
+
+}
